d77e06d3747dae69f2cdc82539a3249768d5ca5f
EduardGeras <103679703+EduardGeras@users.noreply.github.com>
2022-06-25 23:10:38 +0500

    Initial commit

---
+++ src/main/java/cky/pro/course2/dz2_4/Application.java
@@ -0,0 +1,13 @@
+package cky.pro.course2.dz2_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
